7a56046af190266ee2bae42abf52673d4ce913c4
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-04-21 13:47:08 +0100

    update

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -22,3 +22,18 @@ function blurring() {
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
+
+/*
+Parameters:
+num: The number we want to map (in this case, load).
+
+in_min and in_max: The input range. This is the original range where num is located.
+
+out_min and out_max: The output range. This is the range where we want to map the value of num.
+
+Why we use scale():
+It maps a number (num) from one range (in_min to in_max) to another range (out_min to out_max). This helps in adjusting values for things like visual effects, where you need a specific range for better control.
+
+For example, if load ranges from 0 to 100, and you want to map it to a range from 30 to 0 for the blur effect, scale() will adjust it accordingly.
+
+*/
