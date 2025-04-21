fc46c5cfda1ad9fd39e47247f97694514e31ea4e
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-04-21 18:34:18 +0100

    update

---
+++ random-choice-picker/script.js
@@ -0,0 +1,29 @@
+const textarea = document.getElementById("textarea");
+const tagsEl = document.getElementById("tags");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = " ";
+    }, 10);
+  }
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = "";
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("p");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
